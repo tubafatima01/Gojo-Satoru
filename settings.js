c1e1ac07f8bab3a12454d2bdfeedf52fb5a9a745
tubafatima01 <112522667+tubafatima01@users.noreply.github.com>
2022-08-31 04:06:33 +0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -19,11 +19,11 @@ global.APIKeys = {
 }
 
 //customize settings\\
-global.owner = ['Your Number']
-global.premium = ['Your Number']
-global.ownernomer = 'Your Number'
-global.ownername = '🐦Star Boy Nexus'
-global.botname = 'Gojo-Satoru'
+global.owner = ['923228182188']
+global.premium = ['923228182188']
+global.ownernomer = '923228182188'
+global.ownername = '🐦Juned Aly'
+global.botname = 'Juned Aly'
 global.footer = '©NexusNwInc.'
 global.ig = 'https://github.com/nexusNw'
 global.region = 'India, South India, Kerala'
